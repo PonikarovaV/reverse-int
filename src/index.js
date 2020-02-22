01f4ee8ae199f41f8de1f321370979d35d0ff4af
Vanda Ponikarova <ponikarova@gmail.com>
2020-02-22 16:31:24 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if (n >= 0) {
+        return Number(String(n).split('').reverse().join(''));
+    }
+    if (n < 0) {
+        let positive = n * -1;
+        return Number(String(positive).split('').reverse().join(''));
+    }
 }
